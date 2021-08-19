5c2e3fe6791e474b808c259b7e58bf1736df6bf0
Kishimi <anton@green-pr.org>
2021-08-19 14:05:51 +0200

    Added _NMS_ADD_SUBDIR macro

--- Source/Data.hpp
+++ Source/Data.hpp
@@ -2,6 +2,13 @@
 
 #include <string>
 
+#define _NMS_ADD_SUBDIR(Parentdir, Subdir) \
+	struct Subdir final { \
+		static std::string path(const std::string &local = "") { \
+			return Parentdir::path(#Subdir"/"); \
+		} \
+	}
+
 /**
  * @brief Data directory
  */
@@ -12,58 +19,10 @@ struct Data final
 		return std::string("Data/") + local;
 	}
 
-	/**
-	 * @brief Data/Audio directory
-	 */
-	struct Audio final
-	{
-		static std::string path(const std::string &local = "")
-		{
-			return Data::path("Audio/") + local;
-		}
-	};
-
-	/**
-	 * @brief Data/Config directory
-	 */
-	struct Config final
-	{
-		static std::string path(const std::string &local = "")
-		{
-			return Data::path("Config/") + local;
-		}
-	};
-
-	/**
-	 * @brief Data/Fonts directory
-	 */
-	struct Fonts final
-	{
-		static std::string path(const std::string &local = "")
-		{
-			return Data::path("Fonts/") + local;
-		}
-	};
-
-	/**
-	 * @brief Data/Other directory
-	 */
-	struct Other final
-	{
-		static std::string path(const std::string &local = "")
-		{
-			return Data::path("Other/") + local;
-		}
-	};
-
-	/**
-	 * @brief Data/Rooms directory
-	 */
-	struct Rooms final
-	{
-		static std::string path(const std::string &local = "")
-		{
-			return Data::path("Rooms/") + local;
-		}
-	};
+	_NMS_ADD_SUBDIR(Data, Audio);
+	_NMS_ADD_SUBDIR(Data, Config);
+	_NMS_ADD_SUBDIR(Data, Fonts);
+	_NMS_ADD_SUBDIR(Data, Images);
+	_NMS_ADD_SUBDIR(Data, Other);
+	_NMS_ADD_SUBDIR(Data, Rooms);
 };
