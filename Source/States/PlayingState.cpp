ace41d716ec3c1a51d55b1a47d1e44976054490e
Kishimi <anton@green-pr.org>
2021-08-20 15:52:44 +0200

    loading tilemap from file now

--- Source/States/PlayingState.cpp
+++ Source/States/PlayingState.cpp
@@ -8,19 +8,11 @@ void PlayingState::OnEnter(GameData &data)
 	
 	Tileset ts;
 	ts.tileSize = Vec2u(8, 8);
-	ts.LoadFromFile(Data::Images::Path("TestTileset.png"));
+	ts.LoadFromFile(Data::Images::Path("Dungeon tileset.png"));
 
-	m_tilemap.Create(Vec2u(32, 32));
+	m_tilemap.LoadFromFile(Data::Maps::Path("Test.map"));
 	m_tilemap.tileset = ts;
 	m_tilemap.tileSize = Vec2f(32.0f, 32.0f);
-	
-	for (u32 y = 0; y < m_tilemap.Size().y; y++)
-	{
-		for (u32 x = 0; x < m_tilemap.Size().x; x++)
-		{
-			m_tilemap.SetTile(Vec2u(x, y), std::rand() % m_tilemap.tileset.NumTiles());
-		}
-	}
 }
 
 void PlayingState::Update(GameData &data)
